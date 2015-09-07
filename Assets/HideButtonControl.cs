d1d8bfb7051efc1df3987a835b1637d828f7abfb
y_okada <yasokada@gmail.com>
2015-09-07 20:07:23 +0900

    add HidePanelTry1()

---
+++ Assets/HideButtonControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideButtonControl : MonoBehaviour {
+
+	void HidePanelTry1(string panelname) {
+		GameObject panel = GameObject.Find (panelname);
+		if (panel == null) {
+			return;
+		}
+		RectTransform rect = panel.GetComponent (typeof(RectTransform)) as RectTransform;
+		Vector2 scale = rect.localScale;
+		scale.x = 0.0f;
+		scale.y = 0.0f;
+		rect.localScale = scale;
+	}
+
+	public void HideButtonClick() {
+		Debug.Log ("hide button click");
+		HidePanelTry1 ("sinePanel");
+		HidePanelTry1 ("cosinePanel");
+	}
+}
