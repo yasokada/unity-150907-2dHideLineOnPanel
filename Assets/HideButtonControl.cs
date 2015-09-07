2753786a6844520c68c138a32a3d7f58f2ab5bdf
y_okada <yasokada@gmail.com>
2015-09-07 20:19:52 +0900

    add Show button

--- Assets/HideButtonControl.cs
+++ Assets/HideButtonControl.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 public class HideButtonControl : MonoBehaviour {
 
-	void HidePanelTry1(string panelname) {
+	void DisplayPanelTry1(string panelname, bool doShow) {
 		GameObject panel = GameObject.Find (panelname);
 		if (panel == null) {
 			return;
 		}
 		RectTransform rect = panel.GetComponent (typeof(RectTransform)) as RectTransform;
 		Vector2 scale = rect.localScale;
-		scale.x = 0.0f;
-		scale.y = 0.0f;
+		if (doShow) {
+			scale.x = 1.0f;
+			scale.y = 1.0f;
+		} else {
+			scale.x = 0.0f;
+			scale.y = 0.0f;
+		}
 		rect.localScale = scale;
 	}
 
 	public void HideButtonClick() {
 		Debug.Log ("hide button click");
-		HidePanelTry1 ("sinePanel");
-		HidePanelTry1 ("cosinePanel");
+		DisplayPanelTry1 ("sinePanel", /* doShow=*/ false);
+		DisplayPanelTry1 ("cosinePanel", /* doShow=*/ false);
+	}
+
+	public void ShowButtonClick() {
+		Debug.Log ("show button click");
+		DisplayPanelTry1 ("sinePanel", /* doShow=*/ true);
+		DisplayPanelTry1 ("cosinePanel", /* doShow=*/ true);
 	}
 }
